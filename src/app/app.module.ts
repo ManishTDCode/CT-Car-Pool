630cf1c5b54a17eb64ce7e63331e2f6de6e76b1c
Pallavi-game-dev <patilpallavi0000@gmail.com>
2023-04-11 12:05:21 +0530

    dashboard updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { CreaterideComponent } from './Components/createride/createride.component';
+import { ViewrideComponent } from './Components/viewride/viewride.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CreaterideComponent,
+    ViewrideComponent
   ],
   imports: [
     BrowserModule,
