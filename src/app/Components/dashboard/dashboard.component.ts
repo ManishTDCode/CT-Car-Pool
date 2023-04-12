878ca53a35fc7fea82c3cd79fe5db58b59e3b64a
Manish Singh3 <manish.singh3@citiustech.com>
2023-04-12 17:57:16 +0530

    logout added

--- src/app/Components/dashboard/dashboard.component.ts
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -4,19 +4,38 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ViewrideComponent } from '../viewride/viewride.component';
 import { CreaterideComponent } from '../createride/createride.component';
-
+import { Auth } from 'aws-amplify';
+import { Router } from '@angular/router';
+import { AlertifyService } from 'src/app/Services/alertService/alertify.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit,AfterViewInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
+  public loading = false;
   @ViewChild(MatPaginator) paginator: MatPaginator | any;
   dataSource = new MatTableDataSource<any>()
 
   displayedColumns: string[] = ["Name", "Mobile No", "Vehicle No", "Ride Type", "Number of Seats Available", "View", "Action"]
 
-  constructor( private dialog: MatDialog,) { }
+  constructor(private dialog: MatDialog, private router: Router, private alertService: AlertifyService) { }
+
+  logOut() {
+    this.loading = true;
+    Auth.signOut()
+      .then(data => {
+        this.alertService.success('Logout Successfull');
+        localStorage.removeItem('userEmail');
+        localStorage.removeItem('idToken');
+        this.router.navigate(['/']);
+        this.loading = false;
+      })
+      .catch(err => {
+        this.alertService.error(err);
+        this.loading = false;
+      });
+  }
 
   ngOnInit(): void {
 
@@ -69,8 +88,8 @@ export class DashboardComponent implements OnInit,AfterViewInit {
         noOfSeats: 3
 
       },
-     
-      
+
+
     ];
     if (this.dataSource.paginator) {
       this.dataSource.paginator = this.paginator;
@@ -83,22 +102,22 @@ export class DashboardComponent implements OnInit,AfterViewInit {
     console.log("this.paginator", this.paginator);
   }
 
-  viewRideDetails(data: any, enterAnimationDuration: string, exitAnimationDuration: string){
+  viewRideDetails(data: any, enterAnimationDuration: string, exitAnimationDuration: string) {
     const dialogRef = this.dialog.open(ViewrideComponent, {
       width: 'auto',
       maxWidth: '100%',
       height: "auto",
-      
+
       enterAnimationDuration,
       exitAnimationDuration,
     });
   }
-  createRide(enterAnimationDuration: string, exitAnimationDuration: string){
+  createRide(enterAnimationDuration: string, exitAnimationDuration: string) {
     const dialogRef = this.dialog.open(CreaterideComponent, {
       width: '452px',
       maxWidth: '100%',
       height: "auto",
-      
+
       enterAnimationDuration,
       exitAnimationDuration,
     });
